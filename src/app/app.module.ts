e1410851e067a38b10600443ad523d7777f18d46
griquieri436 <you@example.com>
2022-03-23 12:43:23 -0300

    commit actualizacion srtvice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { TrabajosComponent } from './componentes/trabajos/trabajos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobremiComponent,
+    EstudiosComponent,
+    TrabajosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
